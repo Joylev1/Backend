1921219d345f09437c659c8b476a4b852ec43829
Joylev1 <141236807+Joylev1@users.noreply.github.com>
2024-12-11 15:45:25 +0530

    db

--- todo-node-SQLite/src/server.js
+++ todo-node-SQLite/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import path, { dirname } from "path"
 import { fileURLToPath } from "url"
+import authRoutes from "./routes/authRoutes.js"
 
 const app = express()
 
@@ -10,10 +11,17 @@ const __filename = fileURLToPath(import.meta.url)
 
 const __dirname = dirname(__filename)
 
+app.use(express.json())
+
+app.use("/auth", authRoutes)
+
 app.use(express.static(path.join(__dirname, "../public")))
 
+
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname, "..", "public", "index.html")
+    console.log("kaizo")
+    // res.sendFile(__dirname, "..", "public", "index.html")
 })
 
 app.listen(PORT, () => {
