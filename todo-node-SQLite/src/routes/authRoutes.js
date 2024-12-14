4cae64ca6d8c3be1a040ef5a4d807b68f6a777a9
Joylev1 <141236807+Joylev1@users.noreply.github.com>
2024-12-14 22:27:34 +0530

    ..

--- todo-node-SQLite/src/routes/authRoutes.js
+++ todo-node-SQLite/src/routes/authRoutes.js
@@ -2,6 +2,7 @@ import express from "express"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 import db from "../db.js"
+import { describe } from "node:test"
 
 
 const router = express.Router()
@@ -9,8 +10,23 @@ const router = express.Router()
 router.post("/register", (req, res) => {
     //const body = req.body
     const { username, password } = req.body
-    console.log(username, password)
-    res.sendStatus(201)
+    const hashedPass = bcrypt.hashSync(password, 8)
+    try {
+        const insertNewUser = db.prepare(`INSERT INTO users (username, password)
+             VALUES (?, ?)`)
+        const result = insertNewUser.run(username, hashedPass)
+
+        // user created now we add a default todo 
+        const defaultTodo = "Hey :) add your first Todo!"
+        const insertNewTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+        insertNewTodo.run(result.lastInsertRowid, defaultTodo)  // the result.lastinsertedrowid links the user_id to the result's id
+
+        // now for the token of the new user 
+    }
+    catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
 })
 
 router.post("/login", (req, res) => {
